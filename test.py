be7af5d131e7832f92555ae9deca5147b70643f0
ferrerama <ferrerama@gmail.com>
2025-08-21 10:46:44 -0600

    2108v1

---
+++ test.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import xlwings as xw
+
+# Cargar el archivo Excel
+archivo_excel = 'ruta/a/tu/archivo.xlsx'
+hoja = xw.Book(archivo_excel).sheets['NombreDeLaHoja']
+# Seleccionar el rango de celdas
+    
+
