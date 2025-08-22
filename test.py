ada7c5f6b7fe3f9a14bd063bf653e0537c3258e0
ferrerama <ferrerama@gmail.com>
2025-08-21 23:08:09 -0600

    2108v2

--- test.py
+++ test.py
@@ -1,9 +1,6 @@
 import pandas as pd
-import xlwings as xw
 
-# Cargar el archivo Excel
-archivo_excel = 'ruta/a/tu/archivo.xlsx'
-hoja = xw.Book(archivo_excel).sheets['NombreDeLaHoja']
-# Seleccionar el rango de celdas
-    
+df = pd.read_csv('2m Sales Records.csv')
+df['Order Date'] = pd.to_datetime(df['Order Date'], format='%m/%d/%Y')
+print(df.head(3))
 
