8902158ea443cfd2f7c4a5ab41fd79d557ef1fa2
ferrerama <ferrerama@gmail.com>
2025-10-03 18:08:32 -0600

    03102025v1

--- test.py
+++ test.py
@@ -1,45 +1,42 @@
 import marimo
 
-__generated_with = "0.16.3"
+__generated_with = "0.16.5"
 app = marimo.App(width="medium")
 
 
 @app.cell
 def _():
     import pandas as pd
-    from pandas.api.types import CategoricalDtype
-
 
     df = pd.read_excel("C://Users/mferrera/Documents/Reportes/PIPSA GUATEMALA/MASTER SALES DETAILS PIPSA GT.xlsm", sheet_name="Ventas_PIPGT Analisis (2)", skiprows=2)
-    df["Mes"] = df["Fecha"].dt.month_name(locale="es_ES")
-    ventas_por_mes = df.groupby("Mes")["TotalVenta"].sum().reset_index().round(2)
+    #df["Mes"] = df["Fecha"].dt.month_name(locale="es_ES")
+    #ventas_por_mes = df.groupby("Mes")["TotalVenta"].sum().reset_index().round(2)
+    #ventas_por_mes
+    df
+    return
 
-    meses_orden = [
-        "Octubre", "Noviembre", "Diciembre", "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio",
-        "Julio", "Agosto", "Septiembre" 
-    ]
 
-    mes_type = CategoricalDtype(categories=meses_orden, ordered=True)
-    ventas_por_mes["Mes"] = ventas_por_mes["Mes"].astype(mes_type)
-    ventas_por_mes_ordenado = ventas_por_mes.sort_values("Mes").reset_index(drop=True)
-    ventas_por_mes_ordenado
-
-    return (ventas_por_mes,)
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r""" """)
+    return
 
 
 @app.cell
-def _(ventas_por_mes):
-    from pandas.api.types import CategoricalDtype
+def _():
+    import marimo as mo
+    return (mo,)
 
-    meses_orden = [
-        "Octubre", "Noviembre", "Diciembre", "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio",
-        "Julio", "Agosto", "Septiembre" 
-    ]
 
-    mes_type = CategoricalDtype(categories=meses_orden, ordered=True)
-    ventas_por_mes["Mes"] = ventas_por_mes["Mes"].astype(mes_type)
-    ventas_por_mes_ordenado = ventas_por_mes.sort_values("Mes").reset_index(drop=True)
-    ventas_por_mes_ordenado
+@app.cell
+def _(mo):
+    _df = mo.sql(
+        f"""
+        SELECT
+            *
+        FROM
+        """
+    )
     return
 
 
